9bc448dbab547835172807c80b65fb242fac9807
birtecaesar <birte.caesar@hsu-hh.de>
2023-06-11 00:33:53 +0200

    recursively merge childen and update merge dict

--- FamilyCreator.py
+++ FamilyCreator.py
@@ -16,69 +16,18 @@ def get_attributes(elem, key):
                 return value
 
 
-def parse_xml_to_dict_old(element):
-    """recursively loop through xml elements and extract information into a dictionary"""
-
-    def _add_child_to_result(child_data, child_name, result):
-        """helper function to add children to the result dict"""
-        if child_name in result:
-            if isinstance(result[child_name], list):
-                result[child_name].append(child_data)
-            else:
-                result[child_name] = [result[child_name], child_data]
-        else:
-            result.update(child_data)
-
-    result = {}
-    if "name" in element.attrib:
-        # all features have a name attribute
-        if element.attrib.get("mandatory", "not_set") == "true":
-            result["mandatory"] = True
-        if element.attrib.get("abstract", "not_set") == "true":
-            result["abstract"] = True
-        if len(element) == 0:
-            return (
-                {element.attrib["name"]: {"tag": element.tag, **result}}
-                if result
-                else None
-            )
-        for child in element:
-            child_data = parse_xml_to_dict(child)
-            child_name = child.attrib.get("name")
-            if child_name is None:
-                continue
-            _add_child_to_result(child_data, child_name, result)
-        return (
-            {element.attrib["name"]: {"tag": element.tag, **result}} if result else None
-        )
-
-    elif element.tag == "struct":
-        # struct is the wrapper around features
-        # we are interested in the features, which are children of struct, hence we continue with the first entry of
-        # struct (struct only has one entry!)
-        child = next(iter(element))
-        child_data = parse_xml_to_dict(child)
-        child_name = child.attrib.get("name")
-        if child_name is None:
-            return result if result else None
-        _add_child_to_result(child_data, child_name, result)
-        return result if result else None
-    else:
-        raise ValueError("I could not parse xml file and have to return empty-handed!")
-
-
 def parse_xml_to_dict(element):
     """recursively loop through xml elements and extract information into a dictionary"""
 
-    def _add_child_to_result(child_data, child_name, result):
+    def _add_child_to_result(data, name, result):
         """helper function to add children to the result dict"""
-        if child_name in result:
-            if isinstance(result[child_name], list):
-                result[child_name].append(child_data)
+        if name in result:
+            if isinstance(result[name], list):
+                result[name].append(data)
             else:
-                result[child_name] = [result[child_name], child_data]
+                result[name] = [result[name], data]
         else:
-            result.update(child_data)
+            result.update(data)
 
     result = {}
     if "name" in element.attrib:
@@ -105,7 +54,6 @@ def parse_xml_to_dict(element):
             if result
             else {}
         )
-
     elif element.tag == "struct":
         # struct is the wrapper around features
         # we are interested in the features, which are children of struct, hence we continue with the first entry of
@@ -169,9 +117,10 @@ def compare_tag_attributes(a1: dict, a2: dict) -> str:
 def compare_mandatory_attributes(a1: dict, a2: dict) -> Optional[bool]:
     mandatory1, mandatory2 = (d.get("mandatory", "") for d in [a1, a2])
     if mandatory1 != mandatory2 or not any(m for m in [mandatory1, mandatory2]):
-        # case 1: mandatories are either different or not set
+        # case 1: mandatory attrs are either different or not set
         mandatory = None
     else:
+        # case 2: both mandatory attrs are equal
         mandatory = True
     return mandatory
 
@@ -187,6 +136,114 @@ def compare_attributes(children1_attrs, children2_attrs):
                 attrs.update({a_key1: a_val1})
             else:
                 attrs.update({a_key1: "and"})
+
+
+def get_avail_keys(d: dict, keys: set) -> set:
+    for k, v in d.items():
+        if k == "attributes":
+            continue
+        if k != "children":
+            keys.add(k)
+        if isinstance(v, dict):
+            get_avail_keys(v, keys)
+    return keys
+
+
+def add_children_to_lowest_available_level(d: dict, f: str, c: dict, a_k: set) -> None:
+    """
+    Recursively search for lowest branch for feature (f) in root dict (d)
+    - if the feature (f) is not yet known in the dict (d), i.e., not in avail_keys (a_k),
+      add it directly to the root dict (d)
+    - if the feature (f) is in available keys (a_k) but not in the highest branch, recursively search for the lowest
+      branch and add the child dict (c) to d[...][f]["children"].
+    """
+    if f not in a_k:
+        d[f] = c
+    else:
+        if f in d:
+            d[f]["children"].update(c)
+        else:
+            for k, v in d.items():
+                add_children_to_lowest_available_level(d[k]["children"], f, c, a_k)
+
+
+def recursively_merge_children(
+    c1: dict, c2: dict, results: dict, feature_name: str
+) -> None:
+    merged_child_dict = merge_children(c1, c2)
+    add_children_to_lowest_available_level(
+        results, feature_name, merged_child_dict, get_avail_keys(results, set())
+    )
+    for feature, values in merged_child_dict.items():
+        children1, children2 = (
+            get_feature_element(d, "children", feature) for d in [c1, c2]
+        )
+        if not children1 and not children2:
+            # we reached the last level of this branch, no further recursion required
+            continue
+        else:
+            recursively_merge_children(children1, children2, results, feature)
+
+
+def merge_children(c1: dict, c2: dict) -> dict:
+    """compare and merge children_dicts (c1 and c2) and update parent_dict (p)"""
+    result_dict = {}
+    all_occurrences = set(list(c1) + list(c2))
+    duplicate_children = set(c1).intersection(set(c2))
+    unique_children = all_occurrences.difference(duplicate_children)
+    for child in duplicate_children:
+        child1_attrs, child2_attrs = (
+            get_feature_element(d, "attributes", child) for d in [c1, c2]
+        )
+        if child1_attrs == child2_attrs:
+            # use attrs of first child
+            attrs = child1_attrs
+        else:
+            # compare tags
+            child_tag = compare_tag_attributes(child1_attrs, child2_attrs)
+            child_abstract = True if child_tag != "feature" else None
+            child_mandatory = compare_mandatory_attributes(child1_attrs, child2_attrs)
+            # fill attrs
+            attrs = {"tag": child_tag}
+            if child_abstract:
+                attrs.update({"abstract": child_abstract})
+            if child_mandatory:
+                attrs.update({"mandatory": child_mandatory})
+        result_dict.update({child: {"attributes": attrs, "children": {}}})
+    for child in unique_children:
+        fm1_names, fm2_names = (find_and_filter_all_keys(c) for c in [c1, c2])
+        really_unique = not (child in fm1_names and child in fm2_names)
+        if really_unique:
+            # case 1: child only occurs in one feature model disregarding the parent feature
+            child1_attrs, child2_attrs = (
+                get_feature_element(d, "attributes", child) for d in [c1, c2]
+            )
+            child_attrs = child1_attrs if child1_attrs else child2_attrs
+            attrs = {k: v for k, v in child_attrs.items() if k != "mandatory"}
+            result_dict.update({child: {"attributes": attrs, "children": {}}})
+        else:
+            # case 2: child occurs in both feature models, but has in different parent
+            # ToDo: not yet clear how to handle this case, needs further discussion
+            pass
+    return result_dict
+
+
+def get_feature_element(d: dict, e: str, k: str = "") -> dict:
+    """get entry (element) for given dict and key
+    if no key is provided, use the first dict key
+    if entry (element) is empty return an empty dict"""
+    if not k:
+        return d[next(iter(d))].get(e, {})
+    else:
+        return d.get(k, {}).get(e, {})
+
+
+def get_element_trees(file1: str, file2: str) -> tuple["Element", "Element"]:
+    tree1 = ET.parse(file1)
+    root1 = tree1.getroot()
+    tree2 = ET.parse(file2)
+    root2 = tree2.getroot()
+    return root1, root2
 
 
 def main():
@@ -216,16 +273,18 @@ def main():
         else:
             # case 2: both root features have differences in their children (on the first child level)
             # => we have to check all children independently
-            # ToDo: fill attrs
+            # ToDo: check the tag for children (and vs or...)
             attrs = {}
     else:
-        raise ValueError(f"Cannot merge empty feature model and more than two should not exist, but got {all_systems=}")
+        raise ValueError(
+            f"Cannot merge empty feature model and more than two should not exist, but got {all_systems=}"
+        )
+
+    # initialize merged_dict with first level (root feature)
     merged_dict = {system: {"attributes": attrs, "children": {}}}
 
-    merged_child_dict = merge_children(children1, children2, merged_dict)
-    for feature, values in merged_child_dict.items():
-        merged_dict[feature]["children"].update(values["children"])
-    merged_dict[system]["children"].update(merged_child_dict)
+    # merge children and add them to merged_dict
+    recursively_merge_children(children1, children2, merged_dict, system)
 
     # Compare each entry and create merged dict
 
@@ -239,7 +298,7 @@ def main():
             else:  # root feature is not equal
                 merged_dict.update({"root": {}})
 
-    ## System/Module/Component is not included in both FMs
+    # System/Module/Component is not included in both FMs
     diff_comp = []
     for x in fm1_keys:
         if x not in fm2_keys:
@@ -269,81 +328,6 @@ def main():
                 pass
         else:
             pass
-
-
-def merge_children(c1: dict, c2: dict, p: dict) -> dict:
-    """compare and merge children_dicts (c1 and c2) and update parent_dict (p)"""
-    result_dict= {}
-    for feature in p.keys():
-        result_dict.update({feature: {"children":{}}})
-        all_occurences = set(list(c1) + list(c2))
-        duplicate_children = set(c1).intersection(set(c2))
-        unique_children = all_occurences.difference(duplicate_children)
-        for child in duplicate_children:
-            child1_attrs, child2_attrs = (
-                get_feature_element(d, "attributes", child)
-                for d in [c1, c2]
-            )
-            if child1_attrs == child2_attrs:
-                # use attrs of first child
-                attrs = child1_attrs
-            else:
-                # compare tags
-                child_tag = compare_tag_attributes(child1_attrs, child2_attrs)
-                child_abstract = True if child_tag != "feature" else None
-                child_mandatory = compare_mandatory_attributes(
-                    child1_attrs, child2_attrs
-                )
-                # fill attrs
-                attrs = {"tag": child_tag}
-                if child_abstract:
-                    attrs.update({"abstract": child_abstract})
-                if child_mandatory:
-                    attrs.update({"mandatory": child_mandatory})
-            # p[feature]["children"].update(
-            #     {child: {"attributes": attrs, "children": {}}}
-            # )
-            result_dict[feature]["children"].update({child: {"attributes": attrs, "children": {}}})
-        for child in unique_children:
-            fm1_names, fm2_names = (
-                find_and_filter_all_keys(c) for c in [c1, c2]
-            )
-            really_unique = not (child in fm1_names and child in fm2_names)
-            if really_unique:
-                # case 1: child only occurs in one feature model disregarding the parent feature
-                child1_attrs, child2_attrs = (
-                    get_feature_element(d, "attributes", child)
-                    for d in [c1, c2]
-                )
-                child_attrs = child1_attrs if child1_attrs else child2_attrs
-                attrs = {k: v for k, v in child_attrs.items() if k != "mandatory"}
-                # p[feature]["children"].update(
-                #     {child: {"attributes": attrs, "children": {}}}
-                # )
-                result_dict[feature]["children"].update({child: {"attributes": attrs, "children": {}}})
-            else:
-                # case 2: child occurs in both feature models, but has in different parent
-                # ToDo: not yet clear how to handle this case, needs further discussion
-                pass
-    return result_dict
-
-
-def get_feature_element(d: dict, e: str, k: str = "") -> dict:
-    """get entry (element) for given dict and key
-    if no key is provided, use the first dict key
-    if entry (element) is empty return an empty dict"""
-    if not k:
-        return d[next(iter(d))].get(e, {})
-    else:
-        return d.get(k, {}).get(e, {})
-
-
-def get_element_trees(file1: str, file2: str) -> tuple["Element", "Element"]:
-    tree1 = ET.parse(file1)
-    root1 = tree1.getroot()
-    tree2 = ET.parse(file2)
-    root2 = tree2.getroot()
-    return root1, root2
 
 
 if __name__ == "__main__":
