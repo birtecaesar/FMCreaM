682fc8d0afefcecc816d844d41b7bc9d29364f7b
birtecaesar <birte.caesar@hsu-hh.de>
2023-06-10 17:10:48 +0200

    initialize feature model creation and merging

---
+++ FMCreator.py
@@ -0,0 +1,197 @@
+import json
+import sys
+import xml.etree.ElementTree as ET
+from SPARQLWrapper import SPARQLWrapper, JSON, SPARQLWrapper2
+from collections import defaultdict
+
+endpoint_url = "http://localhost:7200/repositories/Drehttisch_korrigiert"
+modulesquery = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX vdi2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+
+SELECT ?module ?component
+WHERE {
+    ?component a vdi2206:Component .
+    ?module a vdi2206:Module .
+    ?module vdi2206:consistsOf ?component.
+}"""
+
+systemsquery = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX vdi2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+
+SELECT ?mechatronicsystem ?system
+WHERE {
+    ?system a vdi2206:System .
+    ?mechatronicsystem a vdi2206:System .
+    ?mechatronicsystem vdi2206:consistsOf ?system.
+}
+"""
+
+sysmoquery = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX vdi2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+
+SELECT ?mechatronicsystem ?module
+WHERE {
+    ?module a vdi2206:Module .
+    ?mechatronicsystem a vdi2206:System .
+    ?mechatronicsystem vdi2206:consistsOf ?module.
+}
+"""
+
+syscoquery = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX vdi2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+
+SELECT ?mechatronicsystem ?component ?module
+WHERE {
+    ?component a vdi2206:Component .
+    ?mechatronicsystem a vdi2206:System .
+    ?module a vdi2206:Module .
+    ?mechatronicsystem vdi2206:consistsOf ?component.
+    filter not exists { ?module vdi2206:consistsOf ?component }
+}
+"""
+
+
+def get_sparql_results(endpoint_url, query):
+    sparql = SPARQLWrapper2(endpoint_url)
+    sparql.setQuery(query)
+    # sparql.setReturnFormat(JSON)
+    return sparql.queryAndConvert()
+
+
+def add_to_lowest_layer(child, parents, query_dict):
+    """add separate components to lowest available parent layer"""
+    for k_1, layer_1 in query_dict.items():
+        for k_2, layer_2 in layer_1.items():
+            for k_3, layer_3 in layer_2.items():
+                # check if lowest parent layer is a component
+                if k_3 in parents:
+                    layer_3.append(child)
+                    return
+            # check if lowest parent layer is a module
+            if k_2 in parents:
+                layer_2.update({child: []})
+                return
+        # check if lowest parent layer is a system
+        if k_1 in parents:
+            layer_1.update({child: {}})
+            return
+    # if no layer corresponds to any of the available parents, create a new level 1 layer
+    query_dict.update({child: {}})
+    return
+
+
+def prettify(element, indent="  "):
+    queue = [(0, element)]  # (level, element)
+    while queue:
+        level, element = queue.pop(0)
+        children = [(level + 1, child) for child in list(element)]
+        if children:
+            element.text = "\n" + indent * (level + 1)  # for child open
+        if queue:
+            element.tail = "\n" + indent * queue[0][0]  # for sibling open
+        else:
+            element.tail = "\n" + indent * (level - 1)  # for parent close
+        queue[0:0] = children  # prepend so children come before siblings
+
+
+# get sparql query results
+component_results = get_sparql_results(endpoint_url, modulesquery)
+system_results = get_sparql_results(endpoint_url, systemsquery)
+module_results = get_sparql_results(endpoint_url, sysmoquery)
+components_of_system_results = get_sparql_results(endpoint_url, syscoquery)
+
+# fill hierarchic dictionary of sparql query results
+# - get components
+components = defaultdict(list)
+if component_results:
+    for c_result in component_results.bindings:
+        key = c_result["module"].value
+        value = c_result["component"].value
+        components[key].append(value)
+# - get modules
+modules = defaultdict(dict)
+if module_results:
+    for m_result in module_results.bindings:
+        key = m_result["mechatronicsystem"].value
+        value = m_result["module"].value
+        modules[key].update({value: components.get(value, [])})
+# - get systems
+systems = defaultdict(dict)
+if system_results:
+    for s_result in system_results.bindings:
+        key = s_result["mechatronicsystem"].value
+        value = s_result["system"].value
+        systems[key].update({value: modules.get(value, {})})
+# - get separate components
+separate_components = defaultdict(list)
+if components_of_system_results:
+    for sc_result in components_of_system_results.bindings:
+        key = sc_result["component"].value
+        value = sc_result["mechatronicsystem"].value
+        separate_components[key].append(value)
+    for component, parents in separate_components.items():
+        add_to_lowest_layer(component, parents, systems)
+
+# use the highest available layer as final dict
+final_dict = (
+    systems if systems else modules if modules else components if components else {}
+)
+
+# prettyprint dict
+# for s, ms in final_dict.items():
+#     print(s)
+#     for m, cgs in ms.items():
+#         print(f"|---{m}")
+#         for cg, cs in cgs.items():
+#             print(f"|---|---{cg}")
+#             for c in cs:
+#                 print(f"|---|---|---{c}")
+
+# Define XML structure for the feature model file
+
+fm_xml = ET.Element("featureModel")
+struct = ET.SubElement(fm_xml, "struct")
+for sys_key, sys_values in final_dict.items():
+    if sys_key in separate_components:
+        root = ET.SubElement(struct, "feature", mandatory="true", name=f"{sys_key}")
+    else:
+        root = ET.SubElement(struct, "and", abstract="true", name=f"{sys_key}")
+    for subs_key, subs_values in sys_values.items():
+        if subs_key in separate_components:
+            system = ET.SubElement(
+                root, "feature", mandatory="true", name=f"{subs_key}"
+            )
+        else:
+            system = ET.SubElement(root, "and", abstract="true", name=f"{subs_key}")
+        for mod_key, mod_values in subs_values.items():
+            if mod_key in separate_components:
+                module = ET.SubElement(
+                    system, "feature", mandatory="true", name=f"{mod_key}"
+                )
+            else:
+                module = ET.SubElement(
+                    system, "and", abstract="true", name=f"{mod_key}"
+                )
+            for comp_value in mod_values:
+                ET.SubElement(module, "feature", mandatory="true", name=f"{comp_value}")
+
+prettify(fm_xml)
+
+tree = ET.ElementTree(fm_xml)
+tree.write("sample.xml", encoding="UTF-8", xml_declaration=True)
---
+++ FamilyCreator.py
@@ -0,0 +1,350 @@
+import xml.etree.ElementTree as ET
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from xml.etree.ElementTree import Element
+
+
+def get_attributes(elem, key):
+    """get the attributes and children of a feature"""
+    for elem_key, elem_val in elem.items():
+        if elem_key == key:
+            return elem_val
+        elif isinstance(elem_val, dict):
+            value = get_attributes(elem_val, key)
+            if value is not None:
+                return value
+
+
+def parse_xml_to_dict_old(element):
+    """recursively loop through xml elements and extract information into a dictionary"""
+
+    def _add_child_to_result(child_data, child_name, result):
+        """helper function to add children to the result dict"""
+        if child_name in result:
+            if isinstance(result[child_name], list):
+                result[child_name].append(child_data)
+            else:
+                result[child_name] = [result[child_name], child_data]
+        else:
+            result.update(child_data)
+
+    result = {}
+    if "name" in element.attrib:
+        # all features have a name attribute
+        if element.attrib.get("mandatory", "not_set") == "true":
+            result["mandatory"] = True
+        if element.attrib.get("abstract", "not_set") == "true":
+            result["abstract"] = True
+        if len(element) == 0:
+            return (
+                {element.attrib["name"]: {"tag": element.tag, **result}}
+                if result
+                else None
+            )
+        for child in element:
+            child_data = parse_xml_to_dict(child)
+            child_name = child.attrib.get("name")
+            if child_name is None:
+                continue
+            _add_child_to_result(child_data, child_name, result)
+        return (
+            {element.attrib["name"]: {"tag": element.tag, **result}} if result else None
+        )
+
+    elif element.tag == "struct":
+        # struct is the wrapper around features
+        # we are interested in the features, which are children of struct, hence we continue with the first entry of
+        # struct (struct only has one entry!)
+        child = next(iter(element))
+        child_data = parse_xml_to_dict(child)
+        child_name = child.attrib.get("name")
+        if child_name is None:
+            return result if result else None
+        _add_child_to_result(child_data, child_name, result)
+        return result if result else None
+    else:
+        raise ValueError("I could not parse xml file and have to return empty-handed!")
+
+
+def parse_xml_to_dict(element):
+    """recursively loop through xml elements and extract information into a dictionary"""
+
+    def _add_child_to_result(child_data, child_name, result):
+        """helper function to add children to the result dict"""
+        if child_name in result:
+            if isinstance(result[child_name], list):
+                result[child_name].append(child_data)
+            else:
+                result[child_name] = [result[child_name], child_data]
+        else:
+            result.update(child_data)
+
+    result = {}
+    if "name" in element.attrib:
+        # all features have a name attribute
+        attributes = {"tag": element.tag}
+        if element.attrib.get("mandatory", "not_set") == "true":
+            attributes["mandatory"] = True
+        if element.attrib.get("abstract", "not_set") == "true":
+            attributes["abstract"] = True
+        if len(element) == 0:
+            return (
+                {element.attrib["name"]: {"attributes": attributes, "children": result}}
+                if attributes
+                else {}
+            )
+        for child in element:
+            child_data = parse_xml_to_dict(child)
+            child_name = child.attrib.get("name")
+            if child_name is None:
+                continue
+            _add_child_to_result(child_data, child_name, result)
+        return (
+            {element.attrib["name"]: {"attributes": attributes, "children": result}}
+            if result
+            else {}
+        )
+
+    elif element.tag == "struct":
+        # struct is the wrapper around features
+        # we are interested in the features, which are children of struct, hence we continue with the first entry of
+        # struct (struct only has one entry!)
+        child = next(iter(element))
+        child_data = parse_xml_to_dict(child)
+        child_name = child.attrib.get("name")
+        if child_name is None:
+            return result if result else None
+        _add_child_to_result(child_data, child_name, result)
+        return result if result else None
+    else:
+        raise ValueError("I could not parse xml file and have to return empty-handed!")
+
+
+def get_all_keys(d):
+    """extract all existing features and attribute keys"""
+    for key, value in d.items():
+        yield key
+        if isinstance(value, dict):
+            yield from get_all_keys(value)
+
+
+def find_and_filter_all_keys(fm_dict):
+    """filter all keys to receive features itself, not meta information of the features"""
+    fm_keys = []
+    for x in get_all_keys(fm_dict):
+        not_relevant_list = [
+            "and",
+            "tag",
+            "abstract",
+            "mandatory",
+            "attributes",
+            "children",
+        ]
+        if x in not_relevant_list:
+            continue
+        if x not in fm_keys:
+            fm_keys.append(x)
+    return fm_keys
+
+
+def compare_tag_attributes(a1: dict, a2: dict) -> str:
+    if a1["tag"] == a2["tag"]:
+        # case 1: both tags are identical
+        tag = a1["tag"]
+    else:
+        if any("and" in a["tag"] for a in [a1, a2]):
+            # case 2: any tag="and" => "and"
+            tag = "and"
+        elif any("or" in a["tag"] for a in [a1, a2]):
+            # case 3: not any tag="and", but any tag="or" => "or"
+            tag = "or"
+        else:
+            raise ValueError(
+                f"Cannot handle this combination: {a1['tag']} vs {a2['tag']}"
+            )
+    return tag
+
+
+def compare_mandatory_attributes(a1: dict, a2: dict) -> Optional[bool]:
+    mandatory1, mandatory2 = (d.get("mandatory", "") for d in [a1, a2])
+    if mandatory1 != mandatory2 or not any(m for m in [mandatory1, mandatory2]):
+        # case 1: mandatories are either different or not set
+        mandatory = None
+    else:
+        mandatory = True
+    return mandatory
+
+
+def compare_attributes(children1_attrs, children2_attrs):
+    defaults = {"tag": "and", "mandatory": False, "abstract": False}
+    avail_attrs = set(list(children1_attrs) + list(children2_attrs))
+    attrs = {}
+    for a_key1, a_val1 in children1_attrs.items():
+        if a_key1 == "tag":
+            a_val2 = children2_attrs.get(a_key1, "not_set")
+            if a_val1 == a_val2:
+                attrs.update({a_key1: a_val1})
+            else:
+                attrs.update({a_key1: "and"})
+
+
+def main():
+    # Read XML data from file
+    root1, root2 = get_element_trees("sample.xml", "sampleC.xml")
+
+    # Iterate through each child element and convert XML to nested dictionary
+    fm_dict1 = parse_xml_to_dict(next(iter(root1)))
+    fm_dict2 = parse_xml_to_dict(next(iter(root2)))
+
+    # Compare the two dictionaries
+    # root feature layer
+    fm_dicts = [fm_dict1, fm_dict2]
+    all_systems = list(set(list(fm_dict1) + list(fm_dict2)))
+    attrs1, attrs2 = (get_feature_element(d, "attributes") for d in fm_dicts)
+    children1, children2 = (get_feature_element(d, "children") for d in fm_dicts)
+    system = next(iter(fm_dict1))
+    if len(all_systems) == 1:
+        # both feature models have the same root feature name
+        attrs = {"tag": compare_tag_attributes(attrs1, attrs2), "abstract": True}
+    elif len(all_systems) == 2:
+        # the feature models have different root feature names
+        if sorted(children1) == sorted(children2):
+            # case 1: both root features have exactly the same children (on the first child level)
+            # => we assume that the root features are identical, but have different names
+            attrs = {"tag": compare_tag_attributes(attrs1, attrs2), "abstract": True}
+        else:
+            # case 2: both root features have differences in their children (on the first child level)
+            # => we have to check all children independently
+            # ToDo: fill attrs
+            attrs = {}
+    else:
+        raise ValueError(f"Cannot merge empty feature model and more than two should not exist, but got {all_systems=}")
+    merged_dict = {system: {"attributes": attrs, "children": {}}}
+
+    merged_child_dict = merge_children(children1, children2, merged_dict)
+    for feature, values in merged_child_dict.items():
+        merged_dict[feature]["children"].update(values["children"])
+    merged_dict[system]["children"].update(merged_child_dict)
+
+    # Compare each entry and create merged dict
+
+    fm1_keys = find_and_filter_all_keys(fm_dict1)
+    fm2_keys = find_and_filter_all_keys(fm_dict2)
+    for x in fm1_keys:
+        for y in fm2_keys:
+            if y == x:  # root feature is equal
+                children1 = get_attributes(fm_dict1, x)
+                children2 = get_attributes(fm_dict2, x)
+            else:  # root feature is not equal
+                merged_dict.update({"root": {}})
+
+    ## System/Module/Component is not included in both FMs
+    diff_comp = []
+    for x in fm1_keys:
+        if x not in fm2_keys:
+            diff_comp.append(x)
+        else:
+            for y in fm2_keys:
+                if y not in fm1_keys and y not in diff_comp:
+                    diff_comp.append(y)
+
+    # Set feature to optional
+    for comp in diff_comp:
+        attributes_fm1 = get_attributes(fm_dict1, comp)
+        attributes_fm2 = get_attributes(fm_dict2, comp)
+        if attributes_fm1 is not None:
+            if attributes_fm1.get("mandatory") is not None:
+                for att in attributes_fm1.values():
+                    att["mandatory"] = False
+            else:
+                pass
+        else:
+            pass
+        if attributes_fm2 is not None:
+            if attributes_fm2.get("mandatory") is not None:
+                for att in attributes_fm1.values():
+                    att["mandatory"] = False
+            else:
+                pass
+        else:
+            pass
+
+
+def merge_children(c1: dict, c2: dict, p: dict) -> dict:
+    """compare and merge children_dicts (c1 and c2) and update parent_dict (p)"""
+    result_dict= {}
+    for feature in p.keys():
+        result_dict.update({feature: {"children":{}}})
+        all_occurences = set(list(c1) + list(c2))
+        duplicate_children = set(c1).intersection(set(c2))
+        unique_children = all_occurences.difference(duplicate_children)
+        for child in duplicate_children:
+            child1_attrs, child2_attrs = (
+                get_feature_element(d, "attributes", child)
+                for d in [c1, c2]
+            )
+            if child1_attrs == child2_attrs:
+                # use attrs of first child
+                attrs = child1_attrs
+            else:
+                # compare tags
+                child_tag = compare_tag_attributes(child1_attrs, child2_attrs)
+                child_abstract = True if child_tag != "feature" else None
+                child_mandatory = compare_mandatory_attributes(
+                    child1_attrs, child2_attrs
+                )
+                # fill attrs
+                attrs = {"tag": child_tag}
+                if child_abstract:
+                    attrs.update({"abstract": child_abstract})
+                if child_mandatory:
+                    attrs.update({"mandatory": child_mandatory})
+            # p[feature]["children"].update(
+            #     {child: {"attributes": attrs, "children": {}}}
+            # )
+            result_dict[feature]["children"].update({child: {"attributes": attrs, "children": {}}})
+        for child in unique_children:
+            fm1_names, fm2_names = (
+                find_and_filter_all_keys(c) for c in [c1, c2]
+            )
+            really_unique = not (child in fm1_names and child in fm2_names)
+            if really_unique:
+                # case 1: child only occurs in one feature model disregarding the parent feature
+                child1_attrs, child2_attrs = (
+                    get_feature_element(d, "attributes", child)
+                    for d in [c1, c2]
+                )
+                child_attrs = child1_attrs if child1_attrs else child2_attrs
+                attrs = {k: v for k, v in child_attrs.items() if k != "mandatory"}
+                # p[feature]["children"].update(
+                #     {child: {"attributes": attrs, "children": {}}}
+                # )
+                result_dict[feature]["children"].update({child: {"attributes": attrs, "children": {}}})
+            else:
+                # case 2: child occurs in both feature models, but has in different parent
+                # ToDo: not yet clear how to handle this case, needs further discussion
+                pass
+    return result_dict
+
+
+def get_feature_element(d: dict, e: str, k: str = "") -> dict:
+    """get entry (element) for given dict and key
+    if no key is provided, use the first dict key
+    if entry (element) is empty return an empty dict"""
+    if not k:
+        return d[next(iter(d))].get(e, {})
+    else:
+        return d.get(k, {}).get(e, {})
+
+
+def get_element_trees(file1: str, file2: str) -> tuple["Element", "Element"]:
+    tree1 = ET.parse(file1)
+    root1 = tree1.getroot()
+    tree2 = ET.parse(file2)
+    root2 = tree2.getroot()
+    return root1, root2
+
+
+if __name__ == "__main__":
+    main()
